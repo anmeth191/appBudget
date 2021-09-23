5ae54f7e9788055a37657b4f85592f443ab2115e
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-09-22 20:58:25 -0400

    Add files via upload
    working in the modify conponent, i can not figure out an error about "Warning: Received NaN for the `value` attribute. If this is expected, cast the value to a string."

--- src/components/modifyBill.js
+++ src/components/modifyBill.js
@@ -6,13 +6,12 @@ import axios from 'axios';
 function ModifyBill(props){
      //receive the id and and covert it into int value
      const id = parseInt(props.match.params.id);
-     const [billModify , setBillModify ] = useState({});
      const [billName , setBillName ] = useState('');
-     const[billPayment , setBillPayment] = useState('');
+     const[billPayment , setBillPayment] = useState(0);
      const [billStatus , setBillStatus ] = useState('');
      const [billFrequency , setBillFrequency ] = useState('');
      const [dayModify , setDayModify ] = useState('');
-     const [weekDayModify , setWeekModify ] = useState('');
+     const [weekDayModify , setWeekModify ] = useState('Monday');
      const [monthModify , setMonthModify ] = useState('');
  
 
@@ -21,45 +20,51 @@ function ModifyBill(props){
      const monthsNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
      const weekDays = [' Sunday ',' Monday ' , ' Tuesday ' , ' Wednesday ' , ' Thursday ' , ' Friday ' , ' Saturday '];
 
+   
+
+     useEffect(() => { 
+
+        function getBillToModify(){
+        axios.get(`http://127.0.0.1:8000/modifybill?id=${id}`).then( response =>{ 
+            setBillName(response.data.body.bill_name);
+            setBillPayment(response.data.body.payment_amount);
+            setBillStatus(response.data.body.status_payment);
+            setBillFrequency(response.data.body.frequency_payment);
+            setDayModify(parseInt(response.data.body.day_payment));
+            setMonthModify(response.data.body.month_payment);
+            setWeekModify(response.data.body.weekday_payment);
+        }).catch( error =>{ console.log(error)})
+    }  
+    
+        getBillToModify();
+    
+    }, []);    
 
 
 
-function getBillToModify(){
-    axios.get(`http://127.0.0.1:8000/modifybill?id=${id}`).then( response =>{ 
-        setBillName(response.data.body.bill_name);
-        setBillPayment(response.data.body.payment_amount);
-        setBillStatus(response.data.body.status_payment);
-        setBillFrequency(response.data.body.frequency_payment);
-        setDayModify(response.data.day_payment);
-        setMonthModify(response.data.month_payment);
-        setWeekModify(response.data.week_payment);
-
-
-
-    }).catch( error =>{ console.log(error)})
-}
-
-
-useEffect(() => { getBillToModify() }, []);    
 return(
     <div>
         <Link to='/'>Home</Link>
          <div>
              <div>
-             <input  type="text" value={ billName } onChange={(event)=>{ setBillName(event.target.value)}}/>  
+              <label htmlFor="bill_name">Bill name:  </label>   
+             <input name="bill_name"  type="text" value={ billName } onChange={(event)=>{ setBillName(event.target.value)}}/>  
              </div>
              <div>
-             <input  type="text" value={ billPayment } onChange={(event)=>{ setBillPayment(event.target.value)}}/>  
+             <label htmlFor="bill_amount">Bill amount:  </label>    
+             <input  name="bill_amount" type="number" value={ billPayment } onChange={(event)=>{ setBillPayment(event.target.value)}}/>  
              </div>
 
             <div>
-                <select value={ billFrequency } onChange={ (event )=>{ setBillFrequency(event.target.value)}}>
+            <label htmlFor="bill_payment">Due:  </label>
+                <select name="bill_payment" value={ billFrequency } onChange={ (event )=>{ setBillFrequency(event.target.value)}}>
                 <option value="Monthly">Monthly</option>
                 <option valye="Yearly">Yearly</option>
                 <option value="Weekly">Weekly</option>
                 </select>
             </div>
-            <div>       
+            <div> 
+            <label htmlFor="bill_status">Bill status:  </label>      
                <select value={ billStatus } onChange={(event)=>{ setBillStatus(event.target.value)}}>
                    <option value="Pending">Pending</option>
                    <option value="Payed">Payed</option>
@@ -69,21 +74,25 @@ return(
     
    
 
-     <div>
-     <select value={ dayModify } onChange={( event )=>{ setDayModify(event.target.value)}}>
+     <div style={ (billFrequency === 'Monthly' || billFrequency ==='Yearly')?{ display:"block"}:{display:"none"}}>
+     <label htmlFor="day_payment">Day Payment: </label>   
+     <select name="day_payment"  value={ dayModify } onChange={( event )=>{ setDayModify(event.target.value)}}>
      {daysNumbers.map( element =>{ return(<option key={ element }>{element}</option>)  })}
      </select>
      </div>
 
-     <div>
-     <select value={ weekDayModify } onChange={( event )=>{ setWeekModify(event.target.value)}}>
+     <div style={ (billFrequency === 'Weekly')?{display:"block"}:{display:"none"}}>
+     <label htmlFor="weekday_payment">Weekday Payment: </label>       
+     
+     <select name="weekday_payment"  value={20} onChange={(event)=>{ setWeekModify(event.target.value)}}>
      {weekDays.map( element =>{ return(<option key={ element }>{element}</option>)  })}
      </select>
      </div>
 
-     <div>
-     <select value={ monthModify } onChange={( event )=>{ setMonthModify(event.target.value)}}>
-     {monthsNumbers.map( element =>{ return(<option key={ element }>{element}</option>)  })}
+     <div style={ (billFrequency === 'Yearly' ) ? {display:"block"}:{display:"none"}}>
+     <label htmlFor="month_payment">Month Payment: </label>           
+     <select  name="month_payment" value={ monthModify } onChange={(event)=>{ setMonthModify(event.target.value)}   }>
+     {monthsNumbers.map( (element,index) =>{ return(<option key={ index }>{element}</option>)  })}
      </select>
      </div>
      
